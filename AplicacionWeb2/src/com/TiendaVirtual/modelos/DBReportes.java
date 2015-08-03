780807d26d18c6416f84fced97b24f5e74947ebe
GaboRocker666 <gaborocker666@gmail.com>
2015-08-03 08:10:56 -0500

    Avance Reportes

--- AplicacionWeb2/src/com/TiendaVirtual/modelos/DBReportes.java
+++ AplicacionWeb2/src/com/TiendaVirtual/modelos/DBReportes.java
@@ -20,24 +20,32 @@ public class DBReportes {
 		Connection con = dbmanager.getConection();
 		if(con==null){return lista;}
 		
-		Statement sentencia;
+		Statement sentencia,sentencia2;
 		ResultSet resultados= null;
-		
+		ResultSet resultados2= null;
+		String query2="";
 		String query="";
 		if(criterio.equals("General") ){
-			query = "SELECT Year(ped.fecha) as anio,count(ped.id_pedidos) as ct,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and tu.id_tipousuario=2 and Year(ped.fecha)="+criterio2+" group by anio,p.id_persona order by ct desc limit 10";
+			query = "SELECT Year(ped.fecha) as anio,count(ped.id_pedidos) as ct,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and tu.id_tipousuario=2 and ped.estadoPedido='Realizado' and Year(ped.fecha)="+criterio2+" group by anio,p.id_persona order by ct desc limit 10";
+			try {
+				sentencia= con.createStatement();
+				resultados= sentencia.executeQuery(query);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+			}
 		}
 		else{
-			query = "SELECT Year(ped.fecha) as anio,count(ped.id_pedidos) as ct,cat.descripcion as categoria,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped, categoria as cat,productos as prod, detallepedido as dtped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and ped.id_pedidos=dtped.id_pedidos and dtped.id_productos=prod.id_productos and prod.id_categoria=cat.id_categoria and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and cat.estado='A' and prod.estado='A' and dtped.estado='A' and tu.id_tipousuario=2 and Year(ped.fecha)='"+criterio2+"' group by anio,p.id_persona,cat.descripcion order by ct desc limit 10";
-		}
-		
-		try {
-			sentencia= con.createStatement();
-			resultados= sentencia.executeQuery(query);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+			query2="select id_categoria,nombre_categoria from categoria where estado='A' order by nombre_categoria asc";
+			try {
+				sentencia2= con.createStatement();
+				resultados2= sentencia2.executeQuery(query2);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+			}
 		}
 		
 		ReportesProductos ped= null;				
@@ -67,26 +75,51 @@ public class DBReportes {
 			}
 		}
 		else{
+			
+			
 			try {
-				while (resultados.next()){
-					ped= new ReportesProductos();
-					ped.setFinicial(Integer.toString(resultados.getInt("anio")));
-					ped.setCtpedidos(resultados.getInt("ct"));
-					ped.setNombre(resultados.getString("nombres"));
-					ped.setApellidos(resultados.getString("apellidos"));
-					ped.setCedula(resultados.getString("cedula"));
-					ped.setTelefono(resultados.getString("telefono"));
-					ped.setEmail(resultados.getString("email"));
-					ped.setDireccion(resultados.getString("direccion"));
-					ped.setCategoria(resultados.getString("categoria"));
-					lista.add(ped);
+				while (resultados2.next()){
+					int idcat=resultados2.getInt("id_categoria");
+					query = "SELECT Year(ped.fecha) as anio,count(ped.id_pedidos) as ct,cat.descripcion as categoria,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped, categoria as cat,productos as prod, detallepedido as dtped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and ped.id_pedidos=dtped.id_pedidos and dtped.id_productos=prod.id_productos and prod.id_categoria=cat.id_categoria and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and cat.estado='A' and prod.estado='A' and dtped.estado='A' and tu.id_tipousuario=2  and cat.id_categoria="+idcat+" and ped.estadoPedido='Realizado' and Year(ped.fecha)='"+criterio2+"' group by anio,p.id_persona,cat.descripcion order by ct desc limit 1";
+					try {
+						sentencia= con.createStatement();
+						resultados= sentencia.executeQuery(query);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+					}
 									
+					try {
+						while (resultados.next()){
+							ped= new ReportesProductos();
+							ped.setFinicial(Integer.toString(resultados.getInt("anio")));
+							ped.setCtpedidos(resultados.getInt("ct"));
+							ped.setNombre(resultados.getString("nombres"));
+							ped.setApellidos(resultados.getString("apellidos"));
+							ped.setCedula(resultados.getString("cedula"));
+							ped.setTelefono(resultados.getString("telefono"));
+							ped.setEmail(resultados.getString("email"));
+							ped.setDireccion(resultados.getString("direccion"));
+							ped.setCategoria(resultados.getString("categoria"));
+							lista.add(ped);
+											
+						}
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						System.out.println("Error en recorrer los resultados");
+					}
+					
+					
 				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				System.out.println("Error en recorrer los resultados");
 			}
+			
+			
 		}
 		
 		try {
@@ -108,29 +141,41 @@ public class DBReportes {
 		Connection con = dbmanager.getConection();
 		if(con==null){return lista;}
 		
-		Statement sentencia;
+		Statement sentencia,sentencia2;
 		ResultSet resultados= null;
+		ResultSet resultados2= null;
 		
 		String query="";
+		String query2="";
 		if(criterio.equals("General") ){
-			query = "SELECT Year(ped.fecha) as anio,Month(ped.fecha) as fecha2,count(ped.id_pedidos) as ct,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and u.id_tipousuario=2 and Month(ped.fecha)="+criterio2+" and Year(ped.fecha)="+anio+" group by anio,fecha2,p.id_persona order by ct desc limit 10";
+			query = "SELECT Year(ped.fecha) as anio,Month(ped.fecha) as fecha2,count(ped.id_pedidos) as ct,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and u.id_tipousuario=2 and ped.estadoPedido='Realizado' and Month(ped.fecha)="+criterio2+" and Year(ped.fecha)="+anio+" group by anio,fecha2,p.id_persona order by ct desc limit 10";
+			try {
+				sentencia= con.createStatement();
+				resultados= sentencia.executeQuery(query);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+			}
 		}
 		else{
-			query = "SELECT Year(ped.fecha) as anio,Month(ped.fecha) as fecha2,count(ped.id_pedidos) as ct,cat.descripcion as categoria,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped, categoria as cat,productos as prod, detallepedido as dtped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and ped.id_pedidos=dtped.id_pedidos and dtped.id_productos=prod.id_productos and prod.id_categoria=cat.id_categoria and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and cat.estado='A' and prod.estado='A' and dtped.estado='A' and u.id_tipousuario=2 and Month(ped.fecha)="+criterio2+" and Year(ped.fecha)="+anio+" group by anio,fecha2,p.id_persona,cat.descripcion order by ct desc limit 10";
+			query2="select id_categoria,nombre_categoria from categoria where estado='A' order by nombre_categoria asc";
+			try {
+				sentencia2= con.createStatement();
+				resultados2= sentencia2.executeQuery(query2);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+			}
 		}
 		
-		try {
-			sentencia= con.createStatement();
-			resultados= sentencia.executeQuery(query);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Error en ejecucion de sentencia" + e.getMessage());
-		}
+		
 		
 		ReportesProductos ped= null;				
 		lista= new ArrayList<ReportesProductos>();
 		//recorrer los resultados
+				
 		if(criterio.equals("General") ){
 			try {
 				while (resultados.next()){
@@ -156,26 +201,47 @@ public class DBReportes {
 		}
 		else{
 			try {
-				while (resultados.next()){
-					ped= new ReportesProductos();
-					ped.setFinicial(Integer.toString(resultados.getInt("anio")));
-					ped.setFfinal(ObtenerMes(resultados.getInt("fecha2")));
-					ped.setCtpedidos(resultados.getInt("ct"));
-					ped.setNombre(resultados.getString("nombres"));
-					ped.setApellidos(resultados.getString("apellidos"));
-					ped.setCedula(resultados.getString("cedula"));
-					ped.setTelefono(resultados.getString("telefono"));
-					ped.setEmail(resultados.getString("email"));
-					ped.setDireccion(resultados.getString("direccion"));
-					ped.setCategoria(resultados.getString("categoria"));
-					lista.add(ped);
-									
+				while (resultados2.next()){
+					int idcat=resultados2.getInt("id_categoria");
+					query = "SELECT Year(ped.fecha) as anio,Month(ped.fecha) as fecha2,count(ped.id_pedidos) as ct,cat.descripcion as categoria,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped, categoria as cat,productos as prod, detallepedido as dtped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and ped.id_pedidos=dtped.id_pedidos and dtped.id_productos=prod.id_productos and prod.id_categoria=cat.id_categoria and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and cat.estado='A' and prod.estado='A' and dtped.estado='A' and u.id_tipousuario=2 and ped.estadoPedido='Realizado' and cat.id_categoria="+idcat+" and Month(ped.fecha)="+criterio2+" and Year(ped.fecha)="+anio+" group by anio,fecha2,p.id_persona,cat.descripcion order by ct desc limit 1";
+					try {
+						sentencia= con.createStatement();
+						resultados= sentencia.executeQuery(query);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+					}
+					
+					try {
+						while (resultados.next()){
+							ped= new ReportesProductos();
+							ped.setFinicial(Integer.toString(resultados.getInt("anio")));
+							ped.setFfinal(ObtenerMes(resultados.getInt("fecha2")));
+							ped.setCtpedidos(resultados.getInt("ct"));
+							ped.setNombre(resultados.getString("nombres"));
+							ped.setApellidos(resultados.getString("apellidos"));
+							ped.setCedula(resultados.getString("cedula"));
+							ped.setTelefono(resultados.getString("telefono"));
+							ped.setEmail(resultados.getString("email"));
+							ped.setDireccion(resultados.getString("direccion"));
+							ped.setCategoria(resultados.getString("categoria"));
+							lista.add(ped);
+											
+						}
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						System.out.println("Error en recorrer los resultados");
+					}
 				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				System.out.println("Error en recorrer los resultados");
 			}
+			
+			
 		}
 		
 		try {
@@ -197,25 +263,34 @@ public class DBReportes {
 		Connection con = dbmanager.getConection();
 		if(con==null){return lista;}
 		
-		Statement sentencia;
+		Statement sentencia,sentencia2;
 		ResultSet resultados= null;
+		ResultSet resultados2= null;
 		System.out.println("");
-		String query="";
+		String query=""; String query2="";
 		if(criterio.equals("General") ){
-			query = "SELECT count(ped.id_pedidos) as ct,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and tu.id_tipousuario=2 and ((ped.fecha>='"+criterio2+"') and (ped.fecha<='"+criterio3+"'))  group by p.id_persona order by ct desc limit 10";
+			query = "SELECT count(ped.id_pedidos) as ct,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and tu.id_tipousuario=2 and ped.estadoPedido='Realizado' and ((ped.fecha>='"+criterio2+"') and (ped.fecha<='"+criterio3+"'))  group by p.id_persona order by ct desc limit 10";
+			try {
+				sentencia= con.createStatement();
+				resultados= sentencia.executeQuery(query);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+			}
 		}
 		else{
-			query = "SELECT count(ped.id_pedidos) as ct,cat.descripcion as categoria,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped, categoria as cat,productos as prod, detallepedido as dtped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and ped.id_pedidos=dtped.id_pedidos and dtped.id_productos=prod.id_productos and prod.id_categoria=cat.id_categoria and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and cat.estado='A' and prod.estado='A' and dtped.estado='A' and tu.id_tipousuario=2 and ((ped.fecha>='"+criterio2+"') and (ped.fecha<='"+criterio3+"')) group by p.id_persona,cat.descripcion order by ct desc limit 10";
+			query2="select id_categoria,nombre_categoria from categoria where estado='A' order by nombre_categoria asc";
+			try {
+				sentencia2= con.createStatement();
+				resultados2= sentencia2.executeQuery(query2);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+			}
 		}
 		
-		try {
-			sentencia= con.createStatement();
-			resultados= sentencia.executeQuery(query);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Error en ejecucion de sentencia" + e.getMessage());
-		}
 		
 		ReportesProductos ped= null;				
 		lista= new ArrayList<ReportesProductos>();
@@ -245,19 +320,39 @@ public class DBReportes {
 		}
 		else{
 			try {
-				while (resultados.next()){
-					ped= new ReportesProductos();
-					ped.setFinicial(criterio2);
-					ped.setFfinal(criterio3);
-					ped.setCtpedidos(resultados.getInt("ct"));
-					ped.setNombre(resultados.getString("nombres"));
-					ped.setApellidos(resultados.getString("apellidos"));
-					ped.setCedula(resultados.getString("cedula"));
-					ped.setTelefono(resultados.getString("telefono"));
-					ped.setEmail(resultados.getString("email"));
-					ped.setDireccion(resultados.getString("direccion"));
-					ped.setCategoria(resultados.getString("categoria"));
-					lista.add(ped);
+				while (resultados2.next()){
+					int idcat=resultados2.getInt("id_categoria");
+					query = "SELECT count(ped.id_pedidos) as ct,cat.descripcion as categoria,p.nombres as nombres,p.apellidos as apellidos,p.cedula as cedula,p.telefono as telefono,p.email as email,p.direccion as direccion FROM personas as p,usuario as u, tipousuario as tu,pedidos as ped, categoria as cat,productos as prod, detallepedido as dtped where p.id_persona=u.id_persona and u.id_tipousuario=tu.id_tipousuario and ped.id_usuario=u.id_usuario and ped.id_pedidos=dtped.id_pedidos and dtped.id_productos=prod.id_productos and prod.id_categoria=cat.id_categoria and p.estado='A' and u.estado='A' and tu.estado='A' and ped.estado='A' and cat.estado='A' and prod.estado='A' and dtped.estado='A' and tu.id_tipousuario=2 and ped.estadoPedido='Realizado' and cat.id_categoria="+idcat+" and ((ped.fecha>='"+criterio2+"') and (ped.fecha<='"+criterio3+"')) group by p.id_persona,cat.descripcion order by ct desc limit 1";
+					try {
+						sentencia= con.createStatement();
+						resultados= sentencia.executeQuery(query);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						System.out.println("Error en ejecucion de sentencia" + e.getMessage());
+					}
+					
+					try {
+						while (resultados.next()){
+							ped= new ReportesProductos();
+							ped.setFinicial(criterio2);
+							ped.setFfinal(criterio3);
+							ped.setCtpedidos(resultados.getInt("ct"));
+							ped.setNombre(resultados.getString("nombres"));
+							ped.setApellidos(resultados.getString("apellidos"));
+							ped.setCedula(resultados.getString("cedula"));
+							ped.setTelefono(resultados.getString("telefono"));
+							ped.setEmail(resultados.getString("email"));
+							ped.setDireccion(resultados.getString("direccion"));
+							ped.setCategoria(resultados.getString("categoria"));
+							lista.add(ped);
+											
+						}
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						System.out.println("Error en recorrer los resultados");
+					}
 									
 				}
 			} catch (SQLException e) {
@@ -265,6 +360,8 @@ public class DBReportes {
 				e.printStackTrace();
 				System.out.println("Error en recorrer los resultados");
 			}
+			
+			
 		}
 		
 		try {
